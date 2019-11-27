2b5b9d850812d20badfef8dd26e811624f46b462
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2019-11-27 02:14:46 +0000

    made some changes to the template
    Decided that the template was a bit boring, therefore uploaded current work on the index page. You can just delete stuff that you do not require for your page and add whatever.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@ $(document).ready(function () {
     $('.animated-icon').toggleClass('open');
   });
 });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJkaXl1IiwiYSI6ImNrMzh5ZnQyODBkaG8zZW92MjVmeXYyejEifQ.fA6c4Or3muRUluPhg-OxJA';
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+center: [-2.970721, 56.462018], // starting position [lng, lat]
+zoom: 12 // starting zoom
+});
