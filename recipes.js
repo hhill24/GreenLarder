bbf6b70e996c48edac301458d0fb747878b4216f
ScottyDog5 <55397552+ScottyDog5@users.noreply.github.com>
2019-12-06 08:33:24 +0000

    Attempt at an ingredient storing java script

--- recipes.js
+++ recipes.js
@@ -1,4 +1,5 @@
-function myFunction() {
+function myFunction()
+{
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
@@ -11,5 +12,42 @@ function myFunction() {
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
+  }
+}
+
+function makeList()
+{
+localStorage.recipe = "placeholder";
+
+  document.getElementById("potatoes").innerHTML =  localStorage.recipe="pototoes";
+  document.getElementById("Pancakes").innerHTML =  localStorage.recipe="Pancakes";
+  document.getElementById("CRbunnies").innerHTML =  localStorage.recipe="CRbunnies";
+  document.getElementById("GoldenDrink").innerHTML =  localStorage.recipe="GoldenDrink";
+  document.getElementById("CofeeCreamer").innerHTML =  localStorage.recipe="CoffeeCreamer";
+  document.getElementById("Quinoa").innerHTML =  localStorage.recipe="Quinoa";
+
+  if (recipe="pototoes")
+  {
+
+  }
+  else if(recipe="Pancakes")
+  {
+
+  }
+  else if(recipe="CRbunnies")
+  {
+
+  }
+  else if(recipe="GoldenDrink")
+  {
+
+  }
+  else if(recipe="CoffeeCreamer")
+  {
+
+  }
+  else if(recipe="Quinoa")
+  {
+    
   }
 }
