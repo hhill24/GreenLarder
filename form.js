b877411be5877d1af086a4cb046d965d2f6f4d20
ScottyDog5 <55397552+ScottyDog5@users.noreply.github.com>
2019-12-06 02:04:45 +0000

    Code added to write form values to a text file

--- form.js
+++ form.js
@@ -1,4 +1,37 @@
-$(document).ready(function ()
+
+
+function StoreVeganCrate()
 {
 
+const fs = require('fs')
+
+// Data which will write in a file.
+let value1 = document.get('firstname').value;
+let value2 = document.getElementsByName('email').value;
+let value3 = document.getElementsByName('age').value;
+let value4 = document.getElementsByName('reason').value;
+
+// Write data in 'Output.txt' .
+fs.writeFile('GreenCrateForm.txt',value1 value2 value3 value4 , (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+})
+}
+
+function StoreNewsLetter()
+{
+
+const fs = require('fs')
+
+  // Data which will write in a file.
+let value1 = document.getElementsByName('lastname').value;
+let value2 = document.getElementsByName('email').value;
+let value3 = document.getElementsByName('age').value;
+let value4 = document.getElementsByName('reason').value;
+
+  // Write data in 'Output.txt' .
+fs.writeFile('Newsletter\GreenCrateForm.txt', value1 value2 value3 value4, (err)) => {
+      // In case of a error throw err.
+      if (err) throw err;
+  })
 }
