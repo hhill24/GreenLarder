420bddcf3de2af6fb5f0447d3e17b617123ade4f
ScottyDog5 <55397552+ScottyDog5@users.noreply.github.com>
2019-12-06 10:28:16 +0000

    Fixed the show text program

--- recipes.js
+++ recipes.js
@@ -1,8 +1,8 @@
-function myFunction()
+function myHide()
 {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  var btnText = document.getElementById("Hide");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -15,6 +15,90 @@ function myFunction()
   }
 }
 
+function myHide2()
+{
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("Hide2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myHide3()
+{
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("Hide3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myHide4()
+{
+  var dots = document.getElementById("dots4");
+  var moreText = document.getElementById("more4");
+  var btnText = document.getElementById("Hide4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myHide5()
+{
+  var dots = document.getElementById("dots5");
+  var moreText = document.getElementById("more5");
+  var btnText = document.getElementById("Hide5");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myHide6()
+{
+  var dots = document.getElementById("dots6");
+  var moreText = document.getElementById("more6");
+  var btnText = document.getElementById("Hide6");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
 
 function makeList()
 {
@@ -34,32 +118,22 @@ localStorage.recipe = "placeholder";
   }
   else if(recipe="Pancakes")
   {
-
-    <ul>3 cups of flour</ul>
-    <ul>2 tablespoons of Sugar</ul>
-    <ul>2 tablespoons of Baking powder</ul>
-    <ul>5 teaspoons of cinnamon</ul>
-    <ul>1 1/4 tablespoon of salt</ul>
+    localStorage.ingredient= ingredient + "flour, Sugar, Baking powder, Cinnamon, Salt";
   }
   else if(recipe="CRbunnies")
   {
-    <ul>16 ounce package cinnamon roll dough with glaze </ul>
-    <ul>5 raisins</ul>
+    localStorage.ingredient=ingredient + "raisins, Cinnamon Roll Dough";
   }
   else if(recipe="GoldenDrink")
   {
-    <ul>1 1/2 cups pineapple cubes</ul>
-    <ul>1 1/2 cups of papaya cubes(deseeded)</ul>
+    localStorage.ingredient=ingredient+"Pineapple, Papaya";
   }
   else if(recipe="CoffeeCreamer")
   {
-    <ul>1/2 cup cashews</ul>
-    <ul>4 pitted medjool dates</ul>
-    <ul>1.5 cups of water</ul>
-    <ul>1 pinch salt</ul>
+    localStorage.ingredient=ingredient+"Cashews, Medjool dates, Salt"
   }
   else if(recipe="Quinoa")
   {
-    quinoa
+    localStorage.ingredient=ingredient+"Quinoa";
   }
 }
